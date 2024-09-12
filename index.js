e45fa114a190d4d831e1e7b9a8295c0bd6b0bc46
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-09-12 10:20:35 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.header("accept-language"),
+    software: req.header("user-agent"),
+  });
 });
 
 // listen for requests :)
